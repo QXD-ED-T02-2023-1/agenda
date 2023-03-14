81fe68f8a5c9352b793db1df4c7c798ec6b0d512
Arthur Araruna <ararunaufc@gmail.com>
2023-03-14 17:21:06 -0300

    Declarando agenda e contato inicial
    *OBS*: As `string`s do C++ usam os operadores corretamente, no sentido de
    que a atribuição copia os caracteres pra nós (em contraste ao C, onde
    apenas o ponteiro é copiado). Outros operadores como comparação também
    funciona da maneira que era de se esperar (compara o _conteúdo_ e não o
    endereço).

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
@@ -8,6 +10,21 @@ struct Contato {
 };
 
 int main() {
+    vector<Contato> agenda;
+    Contato primeiro;
+    Contato segundo;
+
+    segundo.nome = "Cicranim dos Santos";
+    segundo.numero = "88888888";
+
+    primeiro.nome = "Fulanim da Silva";
+    primeiro.numero = "999999999";
+
+    agenda.push_back(primeiro);
+    agenda.push_back(segundo);
+
+    cout << agenda[0].nome << "\n" << agenda[0].numero << "\n";
+    cout << agenda[1].nome << "\n" << agenda[1].numero << "\n";
 
     return 0;
 }
